3f2cf149425d80792003c2b1f604cc1cb07ffe43
wyx <wei00161>
2020-05-23 20:51:05 -0500

    complete read & write, remain small bug.

--- zone_test/hm_zone.h
+++ zone_test/hm_zone.h
@@ -19,19 +19,19 @@ namespace leveldb {
 
         //based on Zone id to open file
         //set C++ put pointer according to the write_pointer in zone
-        Status OpenZone();
+        Status OpenZone() override;
 
-        Status CloseZone();
+        Status CloseZone() override;
 
-        Status FinishZone();
+        Status FinishZone() override;
 
-        ZoneInfo ReportZone();
+        ZoneInfo ReportZone() override;
 
-        Status ResetWritePointer();
+        Status ResetWritePointer() override;
 
-        Status Read(ZoneAddress addr, const char* data) {Status s; return s;}
+        Status ZoneRead(ZoneAddress addr,  char* data) override;
 
-        Status Write(ZoneAddress addr, char* data) {Status s; return s;}
+        Status ZoneWrite(ZoneAddress addr, const char* data) override;
 
         string ToString() const {
 
@@ -61,41 +61,41 @@ namespace leveldb {
         HmZoneNamespace();
         ~HmZoneNamespace()= default;
 
-        size_t GetZoneCount() {
+        size_t GetZoneCount() override{
             return zones_.size();
         }
 
         // Create a new zone.
-        Status NewZone();
+        Status NewZone() override;
 
-        Status GetZone(int id, Zone &res_zone) {
+        Status GetZone(int id, Zone &res_zone) override{
             Status status;
             auto it = zones_.find(id);
             if (it != zones_.end()) {
                 res_zone = it->second;
                 status = Status::OK();
             } else {
-                status = Status::NotFound("Zone NotFound status message");
-                std::cout << "[Zone_namespace.h] [RemoveZone] no zone " << id << "to get" << std::endl;
+                status = Status::NotFound("[Zone_namespace.h] [RemoveZone] no zone to get, zone id: "+ to_string(id));
             }
             return status;
         }
 
-        Status RemoveZone(int id) {
+        Status RemoveZone(int id) override{
             Status status;
             auto it = zones_.find(id);
             if (it != zones_.end()) {
                 zones_.erase(it);
                 status = Status::OK();
             } else {
-                status = Status::NotFound("Zone NotFound status message");
-                std::cout << "[Zone_namespace.h] [RemoveZone] no zone " << id << "to remove" << std::endl;
+                status = Status::NotFound("[Zone_namespace.h] [RemoveZone] no zone to remove, zone id: "+ to_string(id));
             }
             return status;
         }
 
-        Status InitZNS(const char* dir_name);
-        Status InitZone(const char *path, const char *filename,  char *filepath);
+        Status InitZNS(const char* dir_name) override;
+        Status InitZone(const char *path, const char *filename,  char *filepath) override;
+        Status Write(ZoneAddress addr, const char* data) override;
+        Status Read(ZoneAddress addr,  char* data) override;
     private:
         int next_zone_id_ = 0;
         std::map<int, HmZone> zones_;
